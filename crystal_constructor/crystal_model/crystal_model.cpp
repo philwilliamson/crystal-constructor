8160ffb69f535b5a20ed6296939254103266d171
philwilliamson <phil@philcodes.com>
2024-12-11 18:35:43 -0500

    Add crystal model member functions for updating basis vectors

--- crystal_constructor/crystal_model/crystal_model.cpp
+++ crystal_constructor/crystal_model/crystal_model.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <format>
 #include <crystal_constructor/crystal_model/crystal_model.h>
 
 namespace crystal_constructor{ namespace crystal_model {
@@ -10,6 +9,21 @@ void CrystalModel::PrintPOSCAR() const {
     std::cout << a_hat.x  << " " << a_hat.y  << " "  << a_hat.z << '\n';
     std::cout << b_hat.x  << " "  << b_hat.y  << " "  << b_hat.z << '\n';
     std::cout << c_hat.x  << " "  << c_hat.y  << " "  << c_hat.z << '\n';
+}
+
+CrystalModel& CrystalModel::UpdateAHat(crystal_constructor::crystal_model::Vector3 vector) {
+    a_hat = vector;
+    return *this;
+}
+
+CrystalModel& CrystalModel::UpdateBHat(crystal_constructor::crystal_model::Vector3 vector) {
+    b_hat = vector;
+    return *this;
+}
+
+CrystalModel& CrystalModel::UpdateCHat(crystal_constructor::crystal_model::Vector3 vector) {
+    c_hat = vector;
+    return *this;
 }
 
 }} // model for storing crystal state
--- crystal_constructor/crystal_model/crystal_model.h
+++ crystal_constructor/crystal_model/crystal_model.h
@@ -11,6 +11,9 @@ class CrystalModel
 public:
     CrystalModel() = default;
     void PrintPOSCAR() const;
+    CrystalModel& UpdateAHat(crystal_constructor::crystal_model::Vector3 vector);
+    CrystalModel& UpdateBHat(crystal_constructor::crystal_model::Vector3 vector);
+    CrystalModel& UpdateCHat(crystal_constructor::crystal_model::Vector3 vector);
 
 private:
     crystal_constructor::crystal_model::Vector3 a_hat{1.0, 0.0, 0.0};
