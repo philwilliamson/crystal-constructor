271d63dff831349c90e0fb9d8a33e050d612fdc3
philwilliamson <phil@philcodes.com>
2024-12-11 18:36:24 -0500

    Add user input options for updating crystal model basis vectors

--- executables/text_based_application.cpp
+++ executables/text_based_application.cpp
@@ -4,6 +4,9 @@
 #include <crystal_constructor/crystal_model/crystal_model.h>
 
 enum class UserCommand {
+    CHANGE_A_HAT,
+    CHANGE_B_HAT,
+    CHANGE_C_HAT,
     PRINT,
     HELP,
     EXIT,
@@ -11,6 +14,9 @@ enum class UserCommand {
 };
 
 UserCommand userCommandStringToEnum(std::string const& usrCmdStr){
+    if (usrCmdStr == "change_a_hat") return UserCommand::CHANGE_A_HAT;
+    if (usrCmdStr == "change_b_hat") return UserCommand::CHANGE_B_HAT;
+    if (usrCmdStr == "change_c_hat") return UserCommand::CHANGE_C_HAT;
     if (usrCmdStr == "print") return UserCommand::PRINT;
     if (usrCmdStr == "help") return UserCommand::HELP;
     if (usrCmdStr == "exit") return UserCommand::EXIT;
@@ -22,6 +28,10 @@ int main(){
     std::string user_command;
     bool end_program = false;
 
+    float user_x{};
+    float user_y{};
+    float user_z{};
+
     crystal_constructor::crystal_model::CrystalModel crystal_model{};
 
     while (!end_program)
@@ -32,11 +42,41 @@ int main(){
 
         switch (userCommandStringToEnum(user_command))
         {
+        case UserCommand::CHANGE_A_HAT:
+            std::cout << "Enter the x, y, and z components of the new a-hat basis vector separated by spaces (e.g. 0.2 0.6 2.3)\n";
+            std::cin >> user_x >> user_y >> user_z;
+            std::cout << '\n';
+
+            crystal_model.UpdateAHat(crystal_constructor::crystal_model::Vector3{user_x, user_y, user_z});
+
+            break;
+
+        case UserCommand::CHANGE_B_HAT:
+            std::cout << "Enter the x, y, and z components of the new b-hat basis vector separated by spaces (e.g. 0.2 0.6 2.3)\n";
+            std::cin >> user_x >> user_y >> user_z;
+            std::cout << '\n';
+
+            crystal_model.UpdateBHat(crystal_constructor::crystal_model::Vector3{user_x, user_y, user_z});
+
+            break;
+
+        case UserCommand::CHANGE_C_HAT:
+            std::cout << "Enter the x, y, and z components of the new c-hat basis vector separated by spaces (e.g. 0.2 0.6 2.3)\n";
+            std::cin >> user_x >> user_y >> user_z;
+            std::cout << '\n';
+
+            crystal_model.UpdateCHat(crystal_constructor::crystal_model::Vector3{user_x, user_y, user_z});
+
+            break;
+        
         case UserCommand::PRINT:
             crystal_model.PrintPOSCAR();
             break;
 
         case UserCommand::HELP:
+            std::cout << "change_a_hat - Update the a-hat basis vector\n";
+            std::cout << "change_b_hat - Update the b-hat basis vector\n";
+            std::cout << "change_c_hat - Update the c-hat basis vector\n";
             std::cout << "print - Prints crystal model state\n";
             std::cout << "help - Shows command options\n";
             std::cout << "exit - Ends program\n\n";
