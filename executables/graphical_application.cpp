058eda2f1995362ede6cacf06bc4a0f3d3622b2b
philwilliamson <phil@philcodes.com>
2024-12-11 23:07:31 -0500

    Add graphical application source and update executables CMakeLists

---
+++ executables/graphical_application.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+int main() {
+
+    // load GLFW and create a Window
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Crystal Constructor", NULL, NULL);
+
+    // check for valid window
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSwapBuffers(window);
+
+    // rendering loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
