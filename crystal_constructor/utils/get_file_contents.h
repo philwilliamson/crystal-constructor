a65a29a824d4260c08119a1a0710b4763b8636ce
philwilliamson <phil@philcodes.com>
2024-12-12 20:39:04 -0500

    Add utils function for reading file contents into a string

---
+++ crystal_constructor/utils/get_file_contents.h
@@ -0,0 +1,15 @@
+#ifndef GET_FILE_CONTENTS_H
+#define GET_FILE_CONTENTS_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cerrno>
+
+namespace crystal_constructor{ namespace utils {
+
+std::string get_file_contents(const char* filename);
+
+}} // various helper functions
+
+#endif
