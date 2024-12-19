1aa98328307bac08ee9f0f384844139d49d8092f
philwilliamson <phil@philcodes.com>
2024-12-19 01:59:58 -0500

    Add Camera class members for updating parameters from mouse input

--- crystal_constructor/opengl_graphics/camera.cpp
+++ crystal_constructor/opengl_graphics/camera.cpp
@@ -13,9 +13,9 @@ glm::mat4 Camera::GetMatrix() const
     );
 
     glm::vec4 up = glm::vec4(0.0f, 0.0f, -1.0f, 1.0f);
-    up = glm::rotate(glm::mat4(1.0f), glm::radians(phi_), glm::vec3(0.0f, 1.0f, 0.0f)) * up;
+    up = glm::rotate(glm::mat4(1.0f), phi_, glm::vec3(0.0f, 1.0f, 0.0f)) * up;
     glm::vec4 right = glm::vec4(glm::cross(glm::vec3(up), glm::vec3(0.0f, 1.0f, 0.0f)), 1.0f);
-    up = glm::rotate(glm::mat4(1.0f),  glm::radians(theta_), glm::vec3(right)) * up;
+    up = glm::rotate(glm::mat4(1.0f),  theta_, glm::vec3(right)) * up;
     
     glm::mat4 viewMat = glm::lookAt(position, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(up));
     
@@ -26,6 +26,46 @@ glm::mat4 Camera::GetMatrix() const
     );
 
     return projectionMat * viewMat;
+}
+
+void Camera::UpdateFromInputs(GLFWwindow* window, float deltaTime)
+{
+    float sensitivity = baseSensitivity_ * deltaTime;
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS && !mouseDown_)
+    {
+        mouseDown_ = true;
+        glfwGetCursorPos(window, &mouseXInitial_, &mouseYInitial_);
+    }
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
+    {
+        mouseDown_ = false;
+    }
+
+    if (mouseDown_)
+    {
+        double mouseX{}, mouseY{};
+        glfwGetCursorPos(window, &mouseX, &mouseY);
+        float phiDiff = sensitivity * (float)(mouseXInitial_ - mouseX);
+        float thetaDiff = sensitivity * (float)(mouseYInitial_ - mouseY);
+
+        phi_ += phiDiff;
+        theta_ += thetaDiff;
+
+        if (theta_ > glm::radians(180.0f))
+        {
+            theta_ = glm::radians(180.0f);
+        }
+        
+        if (theta_ < glm::radians(0.0f))
+        {
+            theta_ = glm::radians(0.0f);
+        }
+
+        mouseXInitial_ = mouseX;
+        mouseYInitial_ = mouseY;
+    }
 }
 
 }} // class for handling camera data
--- crystal_constructor/opengl_graphics/camera.h
+++ crystal_constructor/opengl_graphics/camera.h
@@ -1,6 +1,8 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
 #include <crystal_constructor/opengl_graphics/shader.h>
@@ -14,13 +16,17 @@ public:
 
     glm::mat4 GetMatrix() const;
 
+    void UpdateFromInputs(GLFWwindow* window, float deltaTime);
+
 private:
     float aspect_{};
     float theta_{glm::radians(90.0f)};
     float phi_{glm::radians(0.0f)};
     float radius_{2.0f};
     bool mouseDown_{false};
-    float sensitivityFactor_{0.01f};
+    float baseSensitivity_{0.2f};
+    double mouseXInitial_{};
+    double mouseYInitial_{};
 };
 
 }} // class for handling camera data
