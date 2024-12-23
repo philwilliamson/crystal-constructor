27353b34d1003116308ae5181e11c4d2ef24b45a
philwilliamson <phil@philcodes.com>
2024-12-23 04:30:51 -0500

    Translate cell mesh vertices to center crystal cell

--- crystal_constructor/crystal_model/mesh_crystal_cell_view.cpp
+++ crystal_constructor/crystal_model/mesh_crystal_cell_view.cpp
@@ -32,21 +32,33 @@ CellMeshData MeshCrystalCellView::GetCellMeshData() const
         crystal_constructor::opengl_graphics::Vertex{a_hat_local + c_hat_local},
         crystal_constructor::opengl_graphics::Vertex{a_hat_local + b_hat_local + c_hat_local},
     };
-    
+
+    glm::mat4 translationMat{
+        glm::vec4(1.0f, 0.0f, 0.0f, 0.0f),
+        glm::vec4(0.0f, 1.0f, 0.0f, 0.0f),
+        glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
+        glm::vec4((a_hat_local + b_hat_local + c_hat_local) * -0.5f, 1.0f)
+    };
+
+    for (crystal_constructor::opengl_graphics::Vertex& vertex : verts)
+    {
+        vertex.position = translationMat * glm::vec4(vertex.position, 1.0f);
+    }
+
 	std::vector<GLuint> inds
     {
         0, 1,
-        1, 2,
-        2, 3,
-        3, 0,
-        4, 5,
-        5, 6,
+        0, 2,
+        0, 3,
+        1, 4,
+        2, 4,
+        2, 5,
+        3, 5,
+        1, 6,
+        3, 6,
+        4, 7,
+        5, 7,
         6, 7,
-        7, 4,
-        0, 4,
-        1, 5,
-        2, 6,
-        3, 7,
     };
     
     CellMeshData cellMeshData{verts, inds};
