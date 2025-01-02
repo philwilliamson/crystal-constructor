adb8d2e0cdf58d57ad68480685092adc66868c20
philwilliamson <phil@philcodes.com>
2025-01-01 21:49:09 -0500

    Add method to graphics crystal model view for getting unform data for atoms shader

--- crystal_constructor/crystal_model/graphics_crystal_model_view.cpp
+++ crystal_constructor/crystal_model/graphics_crystal_model_view.cpp
@@ -6,20 +6,20 @@ GraphicsCrystalModelView::GraphicsCrystalModelView(const CrystalModel& crystalMo
 
 crystal_constructor::opengl_graphics::MeshData GraphicsCrystalModelView::GetCellMeshData() const
 {
-    glm::mat3 basisMatrix{
+    glm::mat3 basisChangeMatrix{
         glm::vec3(0.0f, 0.0f, -1.0f),
         glm::vec3(-1.0f, 0.0f, 0.0f),
         glm::vec3(0.0f, 1.0f, 0.0f)
     };
     
     Vector3 a_hat = crystalModel_.GetAHat();
-    glm::vec3 a_hat_local = basisMatrix * glm::vec3{a_hat.x, a_hat.y, a_hat.z};
+    glm::vec3 a_hat_local = basisChangeMatrix * glm::vec3{a_hat.x, a_hat.y, a_hat.z};
     
     Vector3 b_hat = crystalModel_.GetBHat();
-    glm::vec3 b_hat_local = basisMatrix * glm::vec3{b_hat.x, b_hat.y, b_hat.z};
+    glm::vec3 b_hat_local = basisChangeMatrix * glm::vec3{b_hat.x, b_hat.y, b_hat.z};
     
     Vector3 c_hat = crystalModel_.GetCHat();
-    glm::vec3 c_hat_local = basisMatrix * glm::vec3{c_hat.x, c_hat.y, c_hat.z};
+    glm::vec3 c_hat_local = basisChangeMatrix * glm::vec3{c_hat.x, c_hat.y, c_hat.z};
 
     std::vector<crystal_constructor::opengl_graphics::Vertex> verts
     {
@@ -89,6 +89,63 @@ glm::mat4  GraphicsCrystalModelView::GetModelMatrix() const
     };
 
     return modelMatrix;
+}
+
+const std::vector<AtomDrawData> GraphicsCrystalModelView::GetAtomsUniformData() const
+{
+    std::vector<AtomDrawData> atomDrawData{};
+
+    glm::mat3 basisChangeMatrix{
+        glm::vec3(0.0f, 0.0f, -1.0f),
+        glm::vec3(-1.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f)
+    };
+
+    Vector3 aHat{crystalModel_.GetAHat()};
+    glm::vec3 aHatLocal{basisChangeMatrix * glm::vec3{aHat.x, aHat.y, aHat.z}};
+    Vector3 bHat{crystalModel_.GetBHat()};
+    glm::vec3 bHatLocal{basisChangeMatrix * glm::vec3{bHat.x, bHat.y, bHat.z}};
+    Vector3 cHat{crystalModel_.GetCHat()};
+    glm::vec3 cHatLocal{basisChangeMatrix * glm::vec3{cHat.x, cHat.y, cHat.z}};
+
+    glm::mat4 offsetMatrix{
+        glm::vec4(1.0f, 0.0f, 0.0f, 0.0f),
+        glm::vec4(0.0f, 1.0f, 0.0f, 0.0f),
+        glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
+        glm::vec4((aHatLocal + bHatLocal + cHatLocal) * -0.5f, 1.0f)
+    };
+
+    glm::mat4 scaleMatrix{GetModelMatrix()};
+
+    const std::vector<Atom>& modelAtoms{crystalModel_.GetAtoms()};
+
+    for (int idx = 0; idx < modelAtoms.size(); idx++)
+    {
+        AtomDrawData currentAtomDrawData{};
+        crystal_constructor::crystal_model::Atom currentAtom{modelAtoms.at(idx)};
+        currentAtomDrawData.color = glm::vec3{currentAtom.element.color[0], currentAtom.element.color[1], currentAtom.element.color[2]};
+
+        glm::vec3 atomPosition{
+            currentAtom.position.x * glm::vec3(aHat.x, aHat.y, aHat.z)
+            + currentAtom.position.y * glm::vec3(bHat.x, bHat.y, bHat.z)
+            + currentAtom.position.z * glm::vec3(cHat.x, cHat.y, cHat.z)
+        };
+
+        atomPosition = basisChangeMatrix * atomPosition;
+        
+        glm::mat4 positionMatrix{
+            glm::vec4(1.0f, 0.0f, 0.0f, 0.0f),
+            glm::vec4(0.0f, 1.0f, 0.0f, 0.0f),
+            glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
+            glm::vec4(atomPosition, 1.0f)
+        };
+
+        currentAtomDrawData.modelMatrix = scaleMatrix * positionMatrix * offsetMatrix;
+
+        atomDrawData.push_back(currentAtomDrawData);
+    }
+    
+    return atomDrawData;
 }
 
 }} // class for getting mesh data based on crystal model cell
--- crystal_constructor/crystal_model/graphics_crystal_model_view.h
+++ crystal_constructor/crystal_model/graphics_crystal_model_view.h
@@ -2,13 +2,20 @@
 #define GRAPHICS_CRYSTAL_MODEL_VIEW_H
 
 #include <cmath>
+#include <vector>
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 #include <crystal_constructor/crystal_model/crystal_model.h>
 #include <crystal_constructor/opengl_graphics/mesh_data.h>
 
 namespace crystal_constructor{ namespace crystal_model {
+
+struct AtomDrawData {
+    glm::vec3 color{};
+    glm::mat4 modelMatrix{};
+};
 
 class GraphicsCrystalModelView
 {
@@ -16,7 +23,8 @@ public:
     explicit GraphicsCrystalModelView(const CrystalModel& crystalModel);
 
     crystal_constructor::opengl_graphics::MeshData GetCellMeshData() const;
-    glm::mat4  GetModelMatrix() const;
+    glm::mat4 GetModelMatrix() const;
+    const std::vector<AtomDrawData> GetAtomsUniformData() const;
 
 private:
     const CrystalModel& crystalModel_{};
